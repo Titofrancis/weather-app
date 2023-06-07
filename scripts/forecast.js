88638c7803eec45dac9aab0bb5282a9bc2d7b2ee
Titofrancis <titofrancis01@gmail.com>
2023-06-07 21:36:56 +0100

    Updated forecast URL

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast{
     constructor(){
         this.key = 'ms0pHXl1pWpGXYoRyW02SYGTusAKFLai';
-        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
         //this.cityURI = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${this.key}&q=${this.city}`;
     }
     async updateCity(city){
